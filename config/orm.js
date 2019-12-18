b4ddf72da0118ce2206e06a3624cb80dd196cde8
jhumbrac <john.humbracht@generalassemb.ly>
2019-12-17 23:50:34 -0600

    switched const query to let query

--- config/orm.js
+++ config/orm.js
@@ -2,21 +2,21 @@ const connection = require('./connection.js');
 
 const orm = {
     selectAll: function(cb) {
-        const queryString = "SELECT * FROM burgers";
+        let queryString = "SELECT * FROM burgers";
         connection.query(queryString, (err, result)=> {
             if (err) throw err;
             cb(result);
         });
     },
     insertOne: function(name, cb) {
-        const queryString = `INSERT INTO burgers (burger_name) VALUES ("${name}")`;
+        let queryString = `INSERT INTO burgers (burger_name) VALUES ("${name}")`;
         connection.query(queryString, (err, result)=> {
             if (err) throw err;
             cb(result);
         });
     },
     updateOne: function(condition, cb) {
-        const queryString = `UPDATE burgers SET devoured WHERE id = ${condition}`;
+        let queryString = `UPDATE burgers SET devoured = true WHERE id = ${condition}`;
         connection.query(queryString, (err, result)=>{
             if (err) throw err;
             cb(result);
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const burger = require('../models/burger');
+const burger = require('../models/burger'); // create index.js inside models and require everything
 
 router.get('/', (req, res)=>{
-    burger.selectAll(data=>{
+    burger.selectAll(data=>{ // might have to be req.data
         let handleBarsObj = {
             burgers: data
         };
@@ -11,12 +11,14 @@ router.get('/', (req, res)=>{
     });
 });
 router.post('/api/burgers', (req, res)=> {
-    burger.insertOne()
+    console.log(req.body);
+    burger.insertOne(req.body.name, result=>
+        res.json(result))
 });
 router.put('/api/burgers/:id', (req, res)=>{
     let condition = req.params.id;
-    burger.updateOne()
-})
+    burger.updateOne(condition, result => res.json(result))
+});
 
 
 
